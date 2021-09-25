21a2577d1e2bb6b77b2994954217746da4d9bdda
Fatima <fatimaserikova0@gmail.com>
2021-09-26 01:33:07 +0600

    In process of creating modal

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,18 @@ import '../css/style.css';
 document.querySelector('#app').innerHTML = `
   <h1>Hello, Fatima!</h1>
 `;
+
+const openModal = document.querySelector('.open-modal');
+const modal = document.querySelector('.modal');
+const backdrop = document.querySelector('.modal-backdrop');
+const closeModal = document.querySelector('.close');
+
+openModal.addEventListener('click', () => {
+    modal.style.display = 'block';
+    backdrop.style.display = 'block';
+});
+
+closeModal.addEventListener('click', () => {
+    modal.style.display = 'none';
+    backdrop.style.display = 'none';
+});

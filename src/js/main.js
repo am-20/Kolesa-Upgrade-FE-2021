2c56a1ed31e49e1499cd98282e274c0ff9e346e9
Fatima <fatimaserikova0@gmail.com>
2021-09-26 21:24:43 +0600

    Исправления и доработки

--- src/js/main.js
+++ src/js/main.js
@@ -18,3 +18,21 @@ closeModal.addEventListener('click', () => {
     modal.style.display = 'none';
     backdrop.style.display = 'none';
 });
+
+const colorRadioBtn = document.querySelectorAll('.color_input');
+const colorBlock = document.querySelectorAll('.colors_radio_btn');
+
+const sizeRadioBtn = document.querySelectorAll('.size_input');
+const sizeBlock = document.querySelectorAll('.sizes_radio_btn');
+
+for (let i = 0; i < colorRadioBtn.length; i += 1) {
+    if (colorRadioBtn[i].checked === true) {
+        colorBlock[i].style.background = '#d6ecfb';
+    }
+}
+
+for (let i = 0; i < sizeRadioBtn.length; i += 1) {
+    if (sizeRadioBtn[i].checked === true) {
+        sizeBlock[i].style.background = '#d6ecfb';
+    }
+}
